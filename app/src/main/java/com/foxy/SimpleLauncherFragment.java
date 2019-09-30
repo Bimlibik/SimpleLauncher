3638813f41e5e694cfefbbdbb44da7a6b64bb6ef
Olesya Gorbacheva <554476744j@gmail.com>
2019-09-30 16:22:01 +0800

    добавлен адаптер, отображение списка приложений

--- app/src/main/java/com/foxy/SimpleLauncherFragment.java
+++ app/src/main/java/com/foxy/SimpleLauncherFragment.java
@@ -1,9 +1,14 @@
 package com.foxy;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +16,13 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class SimpleLauncherFragment extends Fragment {
+
+    private static final String TAG = "SimpleLauncherFragment";
 
     private RecyclerView recyclerView;
 
@@ -25,6 +36,76 @@ public class SimpleLauncherFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_simple_launcher, container, false);
         recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setupAdapter();
         return view;
+    }
+
+    // получение списка activities  по заданному intent'у от PackageManager'а
+    // установка адаптера
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        //сортировка в алфовитном порядке
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        a.loadLabel(pm).toString(),
+                        b.loadLabel(pm).toString());
+            }
+        });
+
+        Log.i(TAG, "Найдено: " + activities.size() + " activities.");
+
+        recyclerView.setAdapter(new ActivityAdapter(activities));
+    }
+
+    private class ActivityHolder extends RecyclerView.ViewHolder {
+        private ResolveInfo resolveInfo;
+        private TextView nameTextView;
+
+        public ActivityHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = (TextView) itemView;
+        }
+
+        public void bind(ResolveInfo resolveInfo) {
+            this.resolveInfo = resolveInfo;
+            PackageManager pm = getActivity().getPackageManager();
+            String appName = this.resolveInfo.loadLabel(pm).toString();
+            nameTextView.setText(appName);
+        }
+    }
+
+    private class ActivityAdapter extends RecyclerView.Adapter<ActivityHolder> {
+        private final List<ResolveInfo> activities;
+
+        public ActivityAdapter(List<ResolveInfo> activities) {
+            this.activities = activities;
+        }
+
+        @NonNull
+        @Override
+        public ActivityHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            View view = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new ActivityHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ActivityHolder holder, int position) {
+            ResolveInfo resolveInfo = activities.get(position);
+            holder.bind(resolveInfo);
+        }
+
+        @Override
+        public int getItemCount() {
+            return activities.size();
+        }
     }
 }
